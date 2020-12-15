7f973c2c50f88b4d3d3c002af9f575f50c07f4de
TynClause <elvinnurfurqon17@gmail.com>
2020-12-15 15:59:58 +0700

    add screen volleyball

---
+++ app/src/main/java/com/namaka/CountingScore/volleyBallCounter.kt
@@ -0,0 +1,11 @@
+package com.namaka.CountingScore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class volleyBallCounter : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_volley_ball_counter)
+    }
+}
